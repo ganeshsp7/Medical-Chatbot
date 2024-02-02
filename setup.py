eacde1457c68564c87b3289a27d927e32c005c63
ganesh <ganeshsp7@gmail.com>
2024-02-02 17:45:25 +0530

    setup file edited

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Ganesh',
+    author_email= 'ganeshsp7@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
